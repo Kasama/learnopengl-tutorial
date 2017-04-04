0c682debfffff2790ebffedb422e8dd5b40f049e
Kasama <ralegro@bol.com.br>
2017-04-04 00:09:49 -0300

    added gitignore

--- cmake-build-debug/CMakeFiles/CopyStuff.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CopyStuff"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CopyStuff.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
